9346d993d012cfcb523006f4ce09c5b640d8897e
Eugene Hochenedel <eugeneatschool@gmail.com>
2017-01-18 09:21:16 -0600

    More CNF Expressions
    Added more CNF expressions to the text document.
    Expanded the grammar rules.
    Created lists of the CNF grammar and began checking the lists.

--- Parser.py
+++ Parser.py
@@ -6,27 +6,35 @@ def Main():
 		eachClause = 0;
 		literals = [];
 		openClause = 0;
-		eachNegation = 0;
+		eachNOT = 0;
 		eachAND = 0;
 		eachOR = 0;
 		
+		listNOT = ['?','!',"#"];
+		listOR = ['V','/', ','];
+		listAND = ['&','*',':'];
+		listOPEN = ['(', '[', '<'];
+		listEND = [')', ']', '>'];
+		
+		listNULL = [' ', '\n'];
+		
 		for char in (expression):
-			if (char == "?"):
-				eachNegation += 1;
+			if (char in listNOT):
+				eachNOT += 1;
 			
-			elif (char == "&"):
+			elif (char in listAND):
 				eachAND += 1;
 			
-			elif (char == "V"):
+			elif (char in listOR):
 				eachOR += 1;
 			
-			elif (char == "("):
+			elif (char in listOPEN):
 				openClause += 1;
 			
-			elif (char == ")"):
+			elif (char in listEND):
 				eachClause += 1;
 			
-			elif (char == " " or char == "\n"):
+			elif (char in listNULL):
 				continue;
 			
 			else:
@@ -34,15 +42,15 @@ def Main():
 					continue;
 				literals.append(char);
 		
-		print expression;
-		literals.sort();
 		
+		literals.sort();
+		print expression;
 		print "Literals:", len(literals);
 		print "Each literal:", literals;
 		print "Clauses:", eachClause;
 		print "AND:", eachAND;
 		print "OR:", eachOR;
-		print "NEGATION:", eachNegation;
+		print "NEGATION:", eachNOT;
 		
 		print "\n";
 	
