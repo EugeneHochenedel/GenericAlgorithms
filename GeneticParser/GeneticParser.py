0ecde204525918854d5da5b358f30cd33c3fb20d
Eugene Hochenedel <eugeneatschool@gmail.com>
2017-01-24 17:37:40 -0600

    Convertion and Negation
    Implemented code for converting literals to values. Currently there is
    an issue in which if there is an even number of literals, the values are
    switched from what would be expected.
    Impemented a loop for processing the NOTs in the clause and altering the
    values as need be.

--- GeneticParser/GeneticParser.py
+++ GeneticParser/GeneticParser.py
@@ -1,6 +1,6 @@
 def Main():
 	#Opens and reads the file
-	inFile = open("Expression.txt", 'r')
+	inFile = open("Expression.txt", 'r');
 	
 	#Does the following for every expression in the opened file
 	for expression in (inFile):
@@ -16,7 +16,7 @@ def Main():
 		literals = [];
 		#An empty list that will hold all of the clauses within the string
 		eachClause = [];
-		values = [1, 0, 1, 0];
+		values = [0, 1, 0, 1, 0];
 		indexValue = 0;
 		
 		#List of characters that open a clause in the string
@@ -35,15 +35,14 @@ def Main():
 		#For each char in expression check what they are
 		for char in (expression):
 			#Checks if the char opens a clause
-			if (char in listOPEN):
-				expressionEval += char
+			if (char in listOPEN and openClause == False):
 				#Sets open clause to true
 				openClause = True;
 			
 			#Checks if the char is a NOT and if the char is inside or outside of a clause
 			elif (char in listNOT and openClause == True):
-					#Adds the char to the string for the expression evaluation
-					expressionEval += char;
+				#Adds the char to the string for the expression evaluation
+				expressionEval += char;
 			
 			#Checks if the char is an AND and if the char is inside or outside of a clause
 			elif (char in listAND):
@@ -52,18 +51,16 @@ def Main():
 			
 			#Checks if the char is an OR and if the char is inside or outside of a clause
 			elif (char in listOR and openClause == True):
-					#Adds the char to the string for the expression evaluation
-					expressionEval += char;
+				#Adds the char to the string for the expression evaluation
+				expressionEval += char;
 			
-			#Checks if the character is to be ignored
+			#Checks if the character is the list of characters to be ignored
 			elif (char in listNULL):
-				expressionEval += char
-				#if the character is in the list, it's ignored
-				#continue;
+				#If the character is in the list, it's ignored
+				continue;
 			
 			#Checks if the character closes a clause
 			elif (char in listEND and openClause == True):
-				expressionEval += char
 				#Sets open clause to false, closing it
 				openClause = False;
 				#Adds the informaiton in the expression evaluation string to the list of clauses
@@ -71,7 +68,7 @@ def Main():
 				#Resets the string
 				expressionEval = "";
 
-			elif (openClause == True or openClause == False):
+			elif (openClause == True):
 				#Adds the characters that weren't counted to the expression evaluation string
 				expressionEval += char;
 				#Checks if char is in the list of literals
@@ -80,37 +77,51 @@ def Main():
 					continue;
 				#If the char is not in a list, add it to the list of literals
 				literals.append(char);
-
+		
+		literals.sort();
+				
 		#Loops through each clause and converts the literals to values
-		for string in eachClause:
-			for char in string:
-				if char in listOPEN:
-					convertedExpression += '('
-				elif char in listNOT:
-					convertedExpression += '!';
-				elif char in listAND:
-					convertedExpression += '&&';
-				elif char in listOR:
-					convertedExpression += '||';
-				elif char in listNULL:
-					convertedExpression += ' ';
-				elif char in listEND:
-					convertedExpression += ')';
+		for string in (eachClause):
+			inverseValue = 0;
+			#An empty string to hold the clauses after the values preceded by a NOT are changed
+			inverseExpression = "";
+			
+			for char in (string):
+				if (char in listNOT):
+					convertedExpression += '~';
+				elif (char in listAND):
+					convertedExpression += '&';
+				elif (char in listOR):
+					convertedExpression += '|';
 				else:
-					for item in literals:
-						if item == char:
+					for variable in (literals):
+						if variable == char:
 							indexValue = 0;
 						else:
 							indexValue += 1;
 					convertedExpression += str(values[indexValue])
+
+			for char in (convertedExpression):
+				if (inverseValue == 1):
+					if (char == '1'):
+						inverseExpression += str(0);
+					
+					elif (char == '0'):
+						inverseExpression += str(1);
+					inverseValue = 0;
+					
+				elif (char == '~' and inverseValue == 0):
+					inverseValue = 1;
+				else:
+					inverseExpression += char;
 		
-		literals.sort();
 		print "Expression:", expression;
 		print "Literals:", len(literals);
 		print "Each literal:", literals;
 		print "Clauses:", len(eachClause);
 		print "Conversion:", convertedExpression;
-		print "\n";
+		print "Inverse:", inverseExpression;
+		print '\n';
 	
 	inFile.close();
 
